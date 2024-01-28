b2eb22a4ebe53bfc493a6cbc0b7dc2b2be90d3ac
Abhishek Jaiswal <55433215+abhishekjaj446@users.noreply.github.com>
2024-01-28 13:59:54 +0200

    Audio added to main menu, audiomanager added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
